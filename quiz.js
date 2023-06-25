a07087ef1aa443bc5d6ea20b9a02b5cc2f7eb7c8
Neru-K <atokoam@gmail.com>
2023-06-25 22:46:08 +0900

    fixed bug

--- quiz.js
+++ quiz.js
@@ -33,7 +33,7 @@ class NextElement {
 
   init() {
     this.nextButton.addEventListener("click", this.showQuiz);
-    this.btn.addEventListener("click", function () {
+    this.btn.addEventListener("click", function (this) {
       this.p.display = "block";
     });
     this.displayNextElement();
